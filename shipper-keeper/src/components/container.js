ebeb969de4c51f309308a421175276251a1defa2
Sowmya R <sowmyas@baasha.net>
2021-11-04 22:41:15 -0400

    updated the UI Layout

--- shipper-keeper/src/components/container.js
+++ shipper-keeper/src/components/container.js
@@ -1,30 +1,65 @@
 import * as React from 'react';
 import "./container.module.css";
-import { header, subtitle, container, joinContainer, buttonCenter, text} from "./container.module.css";
+import { header, subtitle, container, joinContainer, buttonCenter, text, greenBg } from "./container.module.css";
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
+import { Row } from 'react-bootstrap'
+import { Col } from 'react-bootstrap';
 
 const Container = (props) => {
     const bannerTitle = props.bannerTitle;
     const bannerSubtitle = props.bannerSubtitle;
     const bannerImage = props.bannerImage;
     const children = props.children;
-    return (
-        <Card className={container}>
-            <Card.Img variant="overlay" src={bannerImage} />
-            <Card.ImgOverlay>
-                <Card.Body>
-                    <Card.Title className={header}>{bannerTitle}</Card.Title>
-                    <Card.Text className={subtitle}>
-                        {bannerSubtitle}
-                    </Card.Text>
-                    <Card.Text className={text}>
-                        {children}
-                    </Card.Text>
+    const imgPos = props.imagePosition;
+    let imagePos = (imgPos == undefined)|| (['left','right'].indexOf(imgPos)==-1)? 'left': imgPos;
+    if (imagePos === 'left') {
+        return (
+            <Card className={container} >
+                {/* <Card.Img variant="overlay" src={bannerImage} style={{ 'opacity': 1 }} /> */}
+                {/* <Card.ImgOverlay> */}
+                <Card.Body className={greenBg}>
+                    <Row>
+                        <Col sm={4} ><img src={bannerImage} /></Col>
+                        <Col sm={8} >
+                            <Card.Title className={header}>{bannerTitle}</Card.Title>
+                            <Card.Text className={subtitle}>{bannerSubtitle}</Card.Text>
+                            <Card.Text className={text} >{children}</Card.Text>
+                            <div className={buttonCenter}>
+                                <Button href="/" variant="link" size="lg"> More >> </Button>
+                            </div>
+                        </Col>
+                    </Row>
+    
                 </Card.Body>
-            </Card.ImgOverlay>
-        </Card>
-    )
+                {/* </Card.ImgOverlay> */}
+            </Card>
+        )
+    } else {
+        return (
+            <Card className={container}>
+                {/* <Card.Img variant="overlay" src={bannerImage} style={{ 'opacity': 1 }} /> */}
+                {/* <Card.ImgOverlay> */}
+                <Card.Body>
+                    <Row>
+                        
+                    <Col sm={8} >
+                        <Card.Title className={header}>{bannerTitle}</Card.Title>
+                        <Card.Text className={subtitle}>{bannerSubtitle}</Card.Text>
+                        <Card.Text className={text} >{children}</Card.Text>
+                        <div className={buttonCenter}>
+                            <Button href="/" variant="link" size="lg"> More >> </Button>
+                        </div>
+                    </Col>
+                    <Col sm={4} ><img src={bannerImage} /></Col>
+                    
+                    </Row>
+    
+                </Card.Body>
+                {/* </Card.ImgOverlay> */}
+            </Card>
+        )
+    }
 }
 
 const JoinContainer = (props) => {
@@ -33,7 +68,7 @@ const JoinContainer = (props) => {
     const bannerImage = props.bannerImage;
     return (
         <Card className={joinContainer}>
-            <Card.Img variant="overlay" src={bannerImage}/>
+            <Card.Img variant="overlay" src={bannerImage} />
             <Card.ImgOverlay>
                 <Card.Body>
                     <Card.Title className={header}>{bannerTitle}</Card.Title>
@@ -41,7 +76,7 @@ const JoinContainer = (props) => {
                         {bannerSubtitle}
                     </Card.Text>
                     <div className={buttonCenter}>
-                        <Button href="/" variant="outline-primary" size="lg">Learn More</Button>
+                        <Button href="/" variant="primary" size="lg">Learn More</Button>
                     </div>
                 </Card.Body>
             </Card.ImgOverlay>
@@ -49,6 +84,6 @@ const JoinContainer = (props) => {
     )
 }
 
-export {JoinContainer};
+export { JoinContainer };
 
 export default Container;
